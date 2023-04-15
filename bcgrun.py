c76053dccab16346373bdb19a8b05da164e30fdc
htylab <10368378+htylab@users.noreply.github.com>
2023-04-15 19:09:45 +0800

    Update bcgrun.py

--- bcgrun.py
+++ bcgrun.py
@@ -20,81 +20,34 @@ import os
 import difflib
 
 
-def shorten_filename(strings):
-    # Initialize a list to store the modified strings
-    modified_strings = []
-    
-    # Iterate through each string in the list
-    for s in strings:
-        # Replace the os.sep character with an underscore
-        modified_string = s.replace(os.sep, '_')
-        modified_strings.append(modified_string)
-    
-    # Initialize a list to store the duplicated substrings
-    duplicates = []
-    
-    # Iterate through each pair of modified strings
-    for i in range(len(modified_strings)):
-        for j in range(i+1, len(modified_strings)):
-            # Use SequenceMatcher to find the longest matching substring
-            matcher = difflib.SequenceMatcher(None, modified_strings[i], modified_strings[j])
-            matches = matcher.get_matching_blocks()
-            
-            # Extract the matching substrings from the modified strings
-            for match in matches:
-                if match.size > 0:
-                    substring = modified_strings[i][match.a:match.a+match.size]
-                    duplicates.append(substring)
-                    
-                    # Replace the duplicated substring with an empty string
-                    modified_strings[i] = modified_strings[i].replace(substring, '')
-                    modified_strings[j] = modified_strings[j].replace(substring, '')
-    
-    # Remove duplicates from the list
-    duplicates = list(set(duplicates))
-    
-    # Initialize a list to store the differences
-    differences = []
-    
-    # Iterate through each modified string and append the non-empty differences
-    for s in modified_strings:
-        if len(s) > 0:
-            differences.append(s)
-    
-    return differences
+def remove_common_substrings(strings):
+    # Split each string by os.sep
+    split_strings = [s.split(os.sep) for s in strings]
+
+    # Find the shortest split string in the list
+    shortest_split_string = min(split_strings, key=len)
+
+    # Initialize common substrings list
+    common_substrings = []
+
+    # Iterate through the elements of the shortest split string
+    for i in range(len(shortest_split_string)):
+        # Check if the element is common in all split strings
+        element = shortest_split_string[i]
+        if all(element in string for string in split_strings):
+            common_substrings.append(element)
+
+    # Remove the common substrings from each split string
+    result = []
+    for string in split_strings:
+        new_string = [part for part in string if part not in common_substrings]
+        result.append(new_string)
+
+    # Join the parts of the strings using os.sep
+    return ['_'.join(parts) for parts in result]
 
 
-def shorten_filenamex(strings):
-    # Initialize a list to store the modified strings
-    modified_strings = []
 
-    # Iterate through each string in the list
-    for s in strings:
-        # Replace the os.sep character with an underscore
-        modified_string = s.replace(os.sep, '_')
-        modified_strings.append(modified_string)
-
-    # Initialize a list to store the duplicated substrings
-    duplicates = []
-
-    # Iterate through each pair of modified strings
-    for i in range(len(modified_strings)):
-        for j in range(i+1, len(modified_strings)):
-            # Use SequenceMatcher to find the longest matching substring
-            matcher = difflib.SequenceMatcher(
-                None, modified_strings[i], modified_strings[j])
-            matches = matcher.get_matching_blocks()
-
-            # Extract the matching substrings from the modified strings
-            for match in matches:
-                if match.size > 0:
-                    substring = modified_strings[i][match.a:match.a+match.size]
-                    duplicates.append(substring)
-
-    # Remove duplicates from the list
-    duplicates = list(set(duplicates))
-
-    return duplicates
 
 
 def main():
@@ -105,6 +58,13 @@ def main():
     parser.add_argument('-o', '--output', default=None, help='Output Path')
     args = parser.parse_args()
 
+
+    #The following is for preparing the output directory
+    #If the user provides an output directory and it does not exist, create it for them
+    #If not provided, the default output directory is the folder of the input file
+
+
+
     f_output_dir = args.output
 
     if f_output_dir is None:
@@ -112,16 +72,20 @@ def main():
     else:
         os.makedirs(f_output_dir, exist_ok=True)
 
+    #Additionally, if the input file is a folder or contains an asterisk
+    # use the glob function to find all input files.
+
     ffs = args.input
     if os.path.isdir(args.input[0]):
-        ffs = glob.glob(join(args.input[0], '*.mat'))
+        ffs = glob.glob(os.path.join(args.input[0], '*.mat'))
 
     elif '*' in args.input[0]:
         ffs = glob.glob(args.input[0])
 
-    short_ffs = shorten_filename(ffs)
+    short_ffs = remove_common_substrings(ffs)
     for ii in range(len(ffs)):
         f = ffs[ii]
+        short_f = short_ffs[ii]
 
         if len(ffs) > 1:
             f_output = short_f.replace('.mat', '_unet.mat')
@@ -131,7 +95,7 @@ def main():
 
         print(f'Processing {f}.....')
         # to create shorter filename for multiple mat files
-        short_f = short_ffs[ii]
+        
         t = time.time()
         mat = h5py.File(f, 'r')
         ECG = np.array(mat['ECG']).flatten()
