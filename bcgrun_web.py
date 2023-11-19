058714b6eb9d66856e306528c6a4ee931411b405
JacobLinCool <jacoblincool@gmail.com>
2023-11-20 01:14:32 +0800

    feat: add data shape description to web interface

--- bcgrun_web.py
+++ bcgrun_web.py
@@ -47,11 +47,18 @@ def run(
 
     return outputs
 
+matlab_file_spec = """
+ECG Data (input_ecg): 1D Numeric Array.
+This array should contain the raw electrocardiogram (ECG) data in shape of (n_points,). The sampling frequency should be 5000 Hz.
+
+EEG Data (input_eeg): 2D Numeric Array.
+Each row corresponds to an EEG channel, and each column corresponds to a time point. We expect 64 EEG channels and 5000 Hz sampling frequency. The shape of the array should be (64, n_points).
+"""
 
 def main():
     app = gr.Interface(
         title="BCG Unet",
-        description="BCGunet: Suppressing BCG artifacts on EEG collected inside an MRI scanner",
+        description="BCGunet: Suppressing BCG artifacts on EEG collected inside an MRI scanner\n\n" + matlab_file_spec,
         fn=run,
         inputs=[
             gr.File(
@@ -59,12 +66,23 @@ def main():
                 type="binary",
                 file_types=["mat"],
                 file_count=["multiple", "directory"],
+                info="Upload MATLAB .mat files. You can specify in-file variable names in the options below.",
             ),
             gr.Slider(
-                label="Learning Rate", minimum=1e-5, maximum=1e-1, step=1e-5, value=1e-3
+                label="Learning Rate",
+                minimum=1e-5,
+                maximum=1e-1,
+                step=1e-5,
+                value=1e-3,
+                info="Adjusts the step size at each iteration of model training.",
             ),
             gr.Slider(
-                label="Window Size (seconds)", minimum=1, maximum=10, step=1, value=2
+                label="Window Size (seconds)",
+                minimum=1,
+                maximum=10,
+                step=1,
+                value=2,
+                info="Sets the time window for analyzing the EEG signal.",
             ),
             gr.Slider(
                 label="Number of Iterations",
@@ -72,10 +90,30 @@ def main():
                 maximum=10000,
                 step=1000,
                 value=5000,
+                info="The total number of passes for training.",
             ),
             gr.Checkbox(
                 label="One Cycle Scheduler",
                 value=True,
+                info="Enable the learning rate policy that cycles the learning rate between two boundaries.",
+            ),
+            gr.Textbox(
+                label="Variable name for ECG (input)",
+                placeholder="Enter the variable name for ECG data",
+                value="ECG",
+                info="Specify the variable name in your .mat file that corresponds to the ECG data.",
+            ),
+            gr.Textbox(
+                label="Variable name for BCG-corrupted EEG (input)",
+                placeholder="Enter the variable name for BCG-corrupted EEG data",
+                value="EEG_before_bcg",
+                info="Specify the variable name in your .mat file that corresponds to the BCG-corrupted EEG data.",
+            ),
+            gr.Textbox(
+                label="Variable name for clean EEG (output)",
+                placeholder="Enter the variable name for clean EEG data",
+                value="EEG_clean",
+                info="Specify the variable name in your .mat file that corresponds to the clean EEG data.",
             ),
         ],
         outputs=[gr.File(label="Output File", file_count="multiple")],
